47f093b5dff56624b744ceaaca2d4c88a13840c6
Sam Subedi <Sam.Suvedi@gmail.com>
2026-02-19 16:10:23 +1000

    Add files via upload

---
+++ ADDS Installation Powershell Script.ps1
@@ -0,0 +1,19 @@
+﻿# Step 1: Install AD DS role and management tools
+Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
+
+# Step 2: Set DSRM password
+$SafeModeAdminPassword = ConvertTo-SecureString "Mango123" -AsPlainText -Force
+
+# Step 3: Promote server to Domain Controller and create new forest abc.com
+Install-ADDSForest `
+    -DomainName "abc.com" `
+    -CreateDnsDelegation:$false `
+    -DatabasePath "C:\Windows\NTDS" `
+    -DomainMode "Default" `
+    -ForestMode "Default" `
+    -InstallDns:$true `
+    -LogPath "C:\Windows\NTDS" `
+    -SysvolPath "C:\Windows\SYSVOL" `
+    -SafeModeAdministratorPassword $SafeModeAdminPassword `
+    -NoRebootOnCompletion:$false `
+    -Force:$true
